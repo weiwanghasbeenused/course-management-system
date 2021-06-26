e61960103c2300e16344944e85b033a8fdbfd8d6
Wei-Hao Wang <wwang03@risd.edu>
2021-06-26 21:07:30 +0800

    add conntent back

---
+++ static/php/connect.php
@@ -0,0 +1,22 @@
+<?
+$hostname_conn = 'localhost';
+$database_conn = '';
+$username_conn = '';
+$password_conn = '';
+try {
+	$pdo = new PDO('mysql:host='.$hostname_conn.'; port=3306; dbname='.$database_conn.'; charset=utf8', $username_conn, $password_conn,
+			array(PDO::ATTR_EMULATE_PREPARES=>false,
+					PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION,
+					PDO::ATTR_PERSISTENT => true
+			)
+	);
+	//$pdo->exec("set names utf8");   // PHP 5.3.6 以前的版本 charset=utf8是無效的，必須使欲用此敘述
+	//echo "成功建立連線(PDO)<br>";
+}
+catch(PDOException $ex){
+	echo 'Error on database: ' . $ex->getMessage() . "\n";
+	echo 'Line: ' . $ex->getLine() . "\n";
+	echo 'Trace: ' . $ex->getTraceAsString() . "\n";
+}
+
+?>
